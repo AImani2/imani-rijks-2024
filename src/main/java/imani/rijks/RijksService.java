7734042afd463873a644e8af884df5f8bba57220
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-05-26 20:54:20 -0400

    trying to create RijsService

--- src/main/java/imani/rijks/RijksService.java
+++ src/main/java/imani/rijks/RijksService.java
@@ -1,12 +1,12 @@
 package imani.rijks;
 
+
+
 public interface RijksService {
     @GET("/api/en/collection")
 
     // acessing the info tourgh artobects - gettnig page query
     // and using the chart to find the things that I need
-    @Query("name") Obect name,
-
     //then make a service factory and a web image class
     // doing the test for number query and artist
 }

458aef1536baa114d581b3141c9832a8a2348aa1
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-05-26 20:58:36 -0400

    RijksServiceFactory

--- src/main/java/imani/rijks/RijksServiceFactory.java
+++ src/main/java/imani/rijks/RijksServiceFactory.java
@@ -6,13 +6,13 @@ public class RijksServiceFactory {
         // use retrofit to give us the service
         // creating a builder and a retrofit object - will always be the same
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://earthquake.usgs.gov/")
+                .baseUrl("https://www.rijksmuseum.nl/")
                 // Configure Retrofit to use Gson to turn the Json into Objects
                 .addConverterFactory(GsonConverterFactory.create())
                 // Configure Retrofit to use Rx
                 .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
                 .build();
 
-        return retrofit.create(EarthquakeService.class);
+        return retrofit.create(RijksService.class);
     }
 }

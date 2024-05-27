b839d71b0218c036ceca75dc8e7d0ce240684c4c
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-05-26 21:52:05 -0400

    still working on the service

--- src/main/java/imani/rijks/RijksService.java
+++ src/main/java/imani/rijks/RijksService.java
@@ -4,6 +4,24 @@ package imani.rijks;
 
 public interface RijksService {
     @GET("/api/en/collection")
+        Single<ArtObjects> page(
+                @Query("key") String key,
+                @Query("p") int page
+    );
+
+    @GET("/api/en/collection")
+    Single<ArtObjects> query(
+            @Query("key") String key,
+            @Query("p") int page,
+            @Query("q") String query
+    );
+
+    @GET("/api/en/collection")
+    Single<ArtObjects> artist(
+            @Query("key") String key,
+            @Query("p") int page,
+            @Query("involvedMaker") string artist
+    );
 
     // acessing the info tourgh artobects - gettnig page query
     // and using the chart to find the things that I need

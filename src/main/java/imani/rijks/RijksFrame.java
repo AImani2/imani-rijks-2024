43cdb5faa246c8abc9fb37425b22900e3f1774ff
AImani2 <121688634+AImani2@users.noreply.github.com>
2024-05-28 14:37:58 -0400

    working on Frame

--- src/main/java/imani/rijks/RijksFrame.java
+++ src/main/java/imani/rijks/RijksFrame.java
@@ -1,6 +1,9 @@
 package imani.rijks;
 
 import com.andrewoid.ApiKey;
+import hu.akarnokd.rxjava3.swing.SwingSchedulers;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,6 +16,7 @@ public class RijksFrame extends JFrame {
     JButton prevButton = new JButton("Previous Page");
     JButton nextButton = new JButton("Next Page");
     JTextField searchBar = new JTextField(42); // Set the preferred width
+    int pageNum = 1;
 
     // create components for the pictures and add them to center
 
@@ -26,14 +30,14 @@ public class RijksFrame extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         JPanel north = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JPanel center = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        //JPanel center = new ImagePanel(new FlowLayout(FlowLayout.CENTER));
 
         north.add(prevButton);
         north.add(searchBar);
         north.add(nextButton);
 
         add(north);
-        add(center);
+        //add(center);
 
         nextButton.addMouseListener(new MouseAdapter() {
             @Override
@@ -68,9 +72,58 @@ public class RijksFrame extends JFrame {
     public void update() {
         // have to create this method
         if(searchBar.getText() == null) {
-            // what am i calling each time?
-
+            if(prevButton.isEnabled()) {
+                fetchAndDisplayPreviousPage();
+            } else if (nextButton.isEnabled()){
+                fetchAndDisplayNextPage();
+            }
+        } else {
+            // do i take the page number into account here too?
+            // should i reset page number to be 1?
+            pageNum = 1;
+            fetchAndDisplaySearch();
         }
+
+    }
+
+    public void fetchAndDisplayPreviousPage() {
+        pageNum--;
+        Disposable disposable = service.page(apiKey.get(), pageNum) // updates automatically according to the website
+                // tells Rx to request the data on a background Thread
+                .subscribeOn(Schedulers.io())
+                // tells Rx to handle the response on Swing's main Thread
+                .observeOn(SwingSchedulers.edt())
+                //.observeOn(AndroidSchedulers.mainThread()) // Instead use this on Android only
+                .subscribe(response -> handleResponse(response),
+                        Throwable::printStackTrace);
+    }
+
+    public void fetchAndDisplayNextPage() {
+        pageNum++;
+        Disposable disposable = service.page(apiKey.get(), pageNum) // updates automatically according to the website
+                // tells Rx to request the data on a background Thread
+                .subscribeOn(Schedulers.io())
+                // tells Rx to handle the response on Swing's main Thread
+                .observeOn(SwingSchedulers.edt())
+                //.observeOn(AndroidSchedulers.mainThread()) // Instead use this on Android only
+                .subscribe(response -> handleResponse(response),
+                        Throwable::printStackTrace);
+    }
+
+    public void fetchAndDisplaySearch() {
+        // how do i know if the user just put in an artist? Do I call artist also?
+        Disposable disposable = service.query(apiKey.get(), pageNum, searchBar.getText()) // updates automatically according to the website
+                // tells Rx to request the data on a background Thread
+                .subscribeOn(Schedulers.io())
+                // tells Rx to handle the response on Swing's main Thread
+                .observeOn(SwingSchedulers.edt())
+                //.observeOn(AndroidSchedulers.mainThread()) // Instead use this on Android only
+                .subscribe(response -> handleResponse(response),
+                        Throwable::printStackTrace);
+    }
+
+    private void handleResponse(ArtObjects response) {
+
     }
 
     public static void main(String[] args) {
